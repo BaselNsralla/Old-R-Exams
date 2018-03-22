83647b917a472aefb4e3fd2e6774af029d3aca0b
Basel <basel.obeid9661@gmail.com>
2018-03-22 18:18:40 +0100

    q2

--- exam_2017_08_14.R
+++ exam_2017_08_14.R
@@ -1,3 +1,6 @@
+library('stringr')
+library('lubridate')
+
 
 q1_a <- function (a, b){
   return((factorial(a)-b^a)^-0.1)
@@ -17,6 +20,36 @@ q1_c <- function() {
 }
 
 q1_d <- function() {
-  ?matrix
   m <- matrix(c(1,2,3,2,1), nrow=5, ncol = 3)
+}
+
+
+q2_a <- function() {
+  rows <- 7
+  for(i in 1:rows){
+    print(rep('xyz', rows-i + 1))
+    print(rep('abc', i))
+  }
+}
+
+q2_b <- function() {
+  print('this is wierd')
+  suc <- 0
+  itr <- 1
+  while(suc <= 50) {
+    suc <- itr + suc
+    itr <- itr + 1
+    if (suc%%2 == 0) {
+      print('GO!')
+    } else if(suc%% 5 == 0){
+      print(sin(suc)) 
+    } else {
+      print(suc)
+    }
+  }
+}
+
+
+q3_a <- function () {
+  
 }
